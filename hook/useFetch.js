142a8ef3fb523957f4eb019c2d4d0cb4fe1cad8f
gabhiinav <gabhiinav@gmail.com>
2024-02-04 21:54:34 +0530

    remove api key

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -10,7 +10,7 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": "5135181e40msh58656ab27c261f9p185456jsnca94c1b5f19c",
+      "X-RapidAPI-Key": "",
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
